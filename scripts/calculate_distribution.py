21eeb29497400db9760ef1d5d62bed060f47dd2b
Michael Egorov <michwill@gmail.com>
2025-10-13 13:07:24 +0200

    Script for distribution: started

---
+++ scripts/calculate_distribution.py
@@ -0,0 +1,62 @@
+from brownie import Contract
+from brownie import config
+from brownie import web3
+from brownie import multicall
+from datetime import datetime
+import json
+from collections import defaultdict
+
+from pprint import pprint
+
+config['autofetch_sources'] = True
+
+START = 23434000
+END = START + 10000  # XXX test only
+SIZE = 500
+BASE = 10**18
+
+
+def main():
+    token_days = defaultdict(float)
+    mc = Contract("0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
+    factory = Contract("0x370a449FeBb9411c95bf897021377fe0B7D100c0")
+    lts = [Contract(factory.markets(i)[3]) for i in range(3)]
+    gauges = [Contract(factory.markets(i)[-1]) for i in range(3)]
+
+    t0 = web3.eth.get_block(START - 1).timestamp
+    users_to_check = [set(), set(), set()]
+    lt_balances = defaultdict(dict)
+    gauge_balances = defaultdict(dict)
+
+    for block in range(START, END - (SIZE - 1), SIZE):
+        for i in range(3):
+            for contract in [lts[i], gauges[i]]:
+                transfers = contract.events.Transfer.get_logs(fromBlock=block, toBlock=block+SIZE-1)
+                users_to_check[i].update(ev.args.sender for ev in transfers)
+                users_to_check[i].update(ev.args.receiver for ev in transfers)
+
+        with multicall(address=mc.address, block_identifier=block+SIZE-1):
+            for i in range(3):
+                for u in users_to_check[i]:
+                    lt_balances[i][u] = lts[i].balanceOf(u)
+                    gauge_balances[i][u] = gauges[i].balanceOf(u)
+            redemption_rates = [g.previewRedeem(10**18) for g in gauges]
+            time = mc.getCurrentBlockTimestamp()
+
+        dt = time - t0
+        t0 = time
+        print(datetime.fromtimestamp(t0))
+
+        for i in range(3):
+            for u in list(users_to_check[i]):
+                if lt_balances[i][u] == 0 and gauge_balances[i][u] == 0:
+                    del lt_balances[i][u]
+                    del gauge_balances[i][u]
+                    users_to_check[i].remove(u)
+
+        # for u, state in user_states.items():
+        #     token_days[u] += state[0] * dt / (86400 * BASE)
+
+        print([len(lb) for lb in lt_balances.values()], [len(gb) for gb in gauge_balances.values()])
+
+    pprint(token_days)
